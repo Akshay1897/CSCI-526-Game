1a79ecae3362c45b25b1ecf9fc1c24727b40ad06
Akshay Bhosle <akshaysbhosle@gmail.com>
2021-02-10 10:29:03 -0800

    smoothened camera and character movement

---
+++ HelloHelloGame/Assets/ThirdPersonMovement.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class ThirdPersonMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public CharacterController controller;
+    public Transform cam;
+
+    public FixedJoystick leftjoystick;
+    public FloatingJoystick rightjoystick;
+    public Animator PlayerAnimator;
+
+    public float speed = 6f;
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
+
+    float lookX = 0;
+    float lookY = 0;
+
+    public CinemachineFreeLook vCam;
+
+    void Start()
+    {
+        CinemachineCore.GetInputAxis = GetAxisCustom;
+    }
+
+    public float GetAxisCustom(string axisName)
+    {
+        if(axisName == "Mouse X")
+        {
+            return lookX;
+        }
+        else if (axisName == "Mouse Y")
+        {
+            return lookY;
+        }
+        return 0;
+    }
+
+
+    void Update()
+    {
+        float horizontal;
+        float vertical;
+
+        if (Input.touchCount > 0) 
+        {
+            horizontal = leftjoystick.Horizontal;
+            vertical = leftjoystick.Vertical;
+
+            lookX = rightjoystick.Horizontal;
+            lookY = rightjoystick.Vertical;
+        }
+        else 
+        {
+            horizontal = Input.GetAxisRaw("Horizontal");
+            vertical = Input.GetAxisRaw("Vertical");
+        }
+
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if (direction.magnitude >= 0.1f) 
+        {
+            PlayerAnimator.SetBool("iswalking", true);
+
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+
+            transform.rotation = Quaternion.Euler(0f, targetAngle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+        }
+        else if(direction.magnitude == 0f) 
+        {
+            PlayerAnimator.SetBool("iswalking", false);
+        }
+
+    }
+}
