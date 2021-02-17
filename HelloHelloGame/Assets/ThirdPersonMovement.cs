89d255d16dcdd3785770ef248bca0ba0626d5aa7
Akshay Bhosle <akshaysbhosle@gmail.com>
2021-02-17 03:57:16 -0800

    animations

--- HelloHelloGame/Assets/ThirdPersonMovement.cs
+++ HelloHelloGame/Assets/ThirdPersonMovement.cs
@@ -20,11 +20,17 @@ public class ThirdPersonMovement : MonoBehaviour
     float lookX = 0;
     float lookY = 0;
 
+    bool jumpButtonPressed;
+
     public CinemachineFreeLook vCam;
+
+    Vector3 direction;
 
     void Start()
     {
-        CinemachineCore.GetInputAxis = GetAxisCustom;
+        //PlayerAnimator = GetComponent<Animator>();
+        //leftjoystick = GetComponent<FixedJoystick>();
+        CinemachineCore.GetInputAxis = GetAxisCustom;       
     }
 
     public float GetAxisCustom(string axisName)
@@ -43,10 +49,12 @@ public class ThirdPersonMovement : MonoBehaviour
 
     void Update()
     {
+
+
         float horizontal;
         float vertical;
 
-        if (Input.touchCount > 0) 
+        if (Input.touchCount > 0)
         {
             horizontal = leftjoystick.Horizontal;
             vertical = leftjoystick.Vertical;
@@ -54,30 +62,75 @@ public class ThirdPersonMovement : MonoBehaviour
             lookX = rightjoystick.Horizontal;
             lookY = rightjoystick.Vertical;
         }
-        else 
+        else
         {
             horizontal = Input.GetAxisRaw("Horizontal");
             vertical = Input.GetAxisRaw("Vertical");
         }
 
-        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+        // If the Button Designated for Jump is Pressed & Player is not Jumping already
 
-        if (direction.magnitude >= 0.1f) 
+
+        direction = new Vector3(horizontal, 0f, vertical);
+
+
+
+        if (direction.magnitude >= 0.1f)  // Walking Mode
         {
-            PlayerAnimator.SetBool("iswalking", true);
-
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
 
             transform.rotation = Quaternion.Euler(0f, targetAngle, 0f);
 
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            controller.Move(moveDir.normalized * speed * Time.deltaTime);
-        }
-        else if(direction.magnitude == 0f) 
-        {
-            PlayerAnimator.SetBool("iswalking", false);
+            controller.Move(moveDir.normalized * speed * direction.magnitude * Time.deltaTime);
         }
 
+        if (jumpButtonPressed)
+        {
+            PlayerAnimator.SetBool("isJumping", true);
+        }
+        else
+        { 
+            PlayerAnimator.SetBool("isJumping", false);
+        }
+    }
+
+    void setAnimation(float magnitude) 
+    {
+        if (magnitude >= 0.1f && magnitude <0.9f)
+        {
+            PlayerAnimator.SetBool("isWalking", true);
+            PlayerAnimator.SetBool("isRunning", false);
+        }
+
+        else if(magnitude >= 0.9f)
+        {
+            PlayerAnimator.SetBool("isWalking", true);
+            PlayerAnimator.SetBool("isRunning", true);
+        }
+
+        else if(magnitude == 0f)  // IDLE Mode
+        {
+            PlayerAnimator.SetBool("isWalking", false);
+            PlayerAnimator.SetBool("isRunning", false);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        setAnimation(direction.magnitude);
+    }
+
+    public void Jump()
+    {
+
+        jumpButtonPressed = true;
+        Invoke("UnJump", 0.1f);
+    }
+
+    public void UnJump()
+    {
+        jumpButtonPressed = false;
     }
 }
