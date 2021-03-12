89c38988c92c3773fe5008db65b52df64d8bbc3c
Akshay Bhosle <akshaysbhosle@gmail.com>
2021-03-12 09:26:49 -0800

    dungeon with start animations

---
+++ HelloHelloGame/Assets/tutscript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tutscript : MonoBehaviour
+{
+
+    public Animator tutAnim;
+    public GameObject fixedJoystick;
+    public GameObject jumpbutton;
+    public GameObject floatJoystick;
+
+
+    void Start()
+    {
+        floatJoystick.SetActive(false);
+        fixedJoystick.SetActive(false);
+        jumpbutton.SetActive(false);
+    }
+
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Invoke("tutorialStart", 3f);
+        
+    }
+
+    public void tutorialStart()
+    {
+        tutAnim.SetBool("playtut", true);
+        floatJoystick.SetActive(true);
+        fixedJoystick.SetActive(true);
+    }
+
+    public void tutorial2()
+    {
+        //tutAnim.SetBool("playJumptut", true);
+        jumpbutton.SetActive(true);
+    }
+}
